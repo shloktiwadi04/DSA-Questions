21659954d87974bef400bc2d060030e8a560834a
shloktiwadi04 <shloktiwadi04@gmail.com>
2026-09-05 11:19:16 +0530

    n times name print

--- prob.cpp
+++ prob.cpp
@@ -1,11 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+
 int main (){
 
-int n=5;
-for( int i=1 ; i<=5 ; i++){
-for(int j=1 ; j<=i ; j++){
-cout<<"*";}
-cout<<endl;}
 
 return 0;}
